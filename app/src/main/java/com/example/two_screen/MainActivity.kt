45e87d935279befced21fc7622714e6c4b1058ce
bagasayn <bagasayn@gmail.com>
2020-10-11 21:43:57 +0300

    fix to run

--- app/src/main/java/com/example/two_screen/MainActivity.kt
+++ app/src/main/java/com/example/two_screen/MainActivity.kt
@@ -3,28 +3,30 @@ package com.example.two_screen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_next.*
 
 
 class MainActivity : AppCompatActivity() {
-
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showCount()
     }
 
-    fun nextActivity(view: View){
+    fun nextActivity() {
         val screenIntent = Intent(
-            this,
+            this@MainActivity,
             NextActivity::class.java
         )
-        val countString = count_text_view.text.toString()
-
-        val count = Integer.parseInt(countString)
-
-        screenIntent.putExtra(NextActivity.TOTAL_COUNT,count)
+        count++
+        screenIntent.putExtra(NextActivity.TOTAL_COUNT, count)
 
         startActivity(screenIntent)
+    }
+    private fun showCount(){
+        val count = intent.getIntExtra(NextActivity.TOTAL_COUNT,0)
+
+        count_text_view.text = count.toString()
     }
 }
--- app/src/main/java/com/example/two_screen/NextActivity.kt
+++ app/src/main/java/com/example/two_screen/NextActivity.kt
@@ -1,11 +1,12 @@
 package com.example.two_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_next.*
 
 class NextActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val TOTAL_COUNT = "total_count"
     }
 
@@ -15,9 +16,21 @@ class NextActivity : AppCompatActivity() {
         showCount()
     }
 
-    private fun showCount(){
-        val count = intent.getIntExtra(TOTAL_COUNT,0)
+    private fun showCount() {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
 
         count_text_view.text = count.toString()
+    }
+
+    fun backActivity() {
+        var count = intent.getIntExtra(TOTAL_COUNT, 0)
+        val screenIntent = Intent(
+            this,
+            NextActivity::class.java
+        )
+        count--
+        screenIntent.putExtra(TOTAL_COUNT, count)
+
+        startActivity(screenIntent)
     }
 }
